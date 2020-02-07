304c5bb1d351428dd917285b452fca3efa3ed99f
Jacky Hsu <2326220@163.com>
2020-02-07 12:03:06 +0800

    feature: complete wechat config

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const crypto = require('crypto')
 const app =express()
 const port = process.env.PORT || 3000;
+const token = 'abcd1234'
 
-app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.get('/',(req,res)=>{
+    console.log('req:', req)
+    console.log('res:', res)
+    const { signature, timestamp, nonce, echostr } = req.query
+    const array = [token , timestamp, nonce]
+    array.sort()
+
+    const tempStr = array.join('')
+    const hashCode = crypto.createHash('sha1')
+    const resultCode = hashCode.update(tempStr,'utf8').digest('hex')
+
+    if(resultCode===signature){
+        res.send(echostr)
+    } else{
+        res.send('mismatch')
+    }
+})
 app.listen(port,()=>console.log('started'))
 
 
