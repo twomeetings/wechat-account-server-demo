d5b67de0e3ae481ff14c52123dde19f2b78df328
Jacky Hsu <2326220@163.com>
2020-02-07 18:09:22 +0800

    chore: compelte menu get and post

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const crypto = require('crypto')
 const fetch = require('node-fetch')
+const morgan=require('morgan')
 const app =express()
 const port = process.env.PORT || 3000;
 const token = 'abcd1234'
 const appID = 'wx511c82e7173ec7df'
 const appsecret = '19ea522e5926127b01b805da6a1890dd'
 
+let access_token =''
 
+app.use(express.json()) // for parsing application/json
+// app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
     console.log('req:', req)
@@ -27,12 +32,47 @@ app.get('/',(req,res)=>{
     }
 })
 
-app.get('/access_token', async (req, res)=>{
-    const response = await fetch(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`)
-    const json = response.json()
-    console.log('==== json:' , json)
-    res.send(json)
+app.get('/access_token',(req, res)=>{
+    fetch(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`)
+    .then(response => response.json())
+    .then(json => {        
+        console.log('=== json:', json)
+        console.log('check nodemon works')
+        access_token=json.access_token
+        res.send(json)
+    })
 })
-app.listen(port,()=>console.log('started'))
+
+app.get('/getMenuItems', (req, res)=>{
+    fetch(`https://api.weixin.qq.com/cgi-bin/menu/get?access_token=${access_token}`)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log('=== json:', json)
+        res.send(json)
+    })
+})
+
+app.post('/updateMenuItems', (req, res)=>{
+    const body = JSON.stringify(req.body)
+    console.log('== body', body)
+    fetch(` https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${access_token}`,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body
+    }).then(r=>{
+        res.send('success')
+        console.log('response', r)
+    }).catch(err=>{
+        console.log('err', err)
+        res.send(err)
+    })
+
+})
+
+app.listen(port,()=>{
+    console.log(`started: http://localhost:${port}`)
+})
 
 
