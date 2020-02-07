eb56b60596e342143be9b313ce0691bcace8f95c
Jacky Hsu <2326220@163.com>
2020-02-07 14:35:24 +0800

    chore: add get access_token api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const crypto = require('crypto')
+const fetch = require('node-fetch')
 const app =express()
 const port = process.env.PORT || 3000;
 const token = 'abcd1234'
+const appID = 'wx511c82e7173ec7df'
+const appsecret = '19ea522e5926127b01b805da6a1890dd'
+
 
 
 app.get('/',(req,res)=>{
@@ -21,6 +25,13 @@ app.get('/',(req,res)=>{
     } else{
         res.send('mismatch')
     }
+})
+
+app.get('/access_token', async (req, res)=>{
+    const res = await fetch(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`)
+    const json = res.json()
+    console.log('==== json:' , json)
+    res.send(json)
 })
 app.listen(port,()=>console.log('started'))
 
